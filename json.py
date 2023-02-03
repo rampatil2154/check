62dd0d10b363642374ab00fd001cfaae82e16879
rampatil2154 <123980852+rampatil2154@users.noreply.github.com>
2023-02-03 08:25:34 +0530

    multi_file

---
+++ json.py
@@ -0,0 +1,6 @@
+import json
+d={'course_name':'python','fees':20000
+    }
+f=json.dumps(d)
+print(type(f))
+print(f)
---
+++ student.py
@@ -0,0 +1,8 @@
+class student:
+   # __name="ravi"
+    def __init__(self):
+     #   print(self.__name)
+        self.__displayinfo()
+    def __displayinfo(self):
+        print("wellcome to ws")
+obj=student()
